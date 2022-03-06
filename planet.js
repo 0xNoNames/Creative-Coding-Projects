cde367712f9cfe7d9531ff05fa435af0e030be29
Arthur Pêtre <arthur30700@gmail.com>
2022-03-07 00:42:25 +0100

    Initial commit

--- planet.js
+++ planet.js
@@ -1,10 +1,22 @@
 class Planet {
-    constructor(radius, distance, orbitSpeed, color) {
+    constructor(radius, distance, npoints, orbitSpeed, color) {
         this.radius = radius;
         this.angle = random(TWO_PI);
         this.distance = distance;
+        this.npoints = npoints;
         this.orbitSpeed = orbitSpeed;
         this.color = color;
+    }
+
+    polygon(x, y, radius, npoints) {
+        let angle = TWO_PI / npoints;
+        beginShape();
+        for (let a = 0; a < TWO_PI; a += angle) {
+            let sx = x + cos(a) * radius;
+            let sy = y + sin(a) * radius;
+            vertex(sx, sy);
+        }
+        endShape(CLOSE);
     }
 
     show() {
@@ -12,7 +24,16 @@ class Planet {
         fill(this.color);
         rotate(this.angle);
         translate(this.distance, 0);
-        ellipse(0, 0, this.radius);
+        this.polygon(0, 0, this.radius, this.npoints);
         pop();
     }
+
+    orbit() {
+        this.angle += this.orbitSpeed;
+    }
+
+    // rotation() {
+    //     push();
+    //     pop();
+    // }
 }
--- sketch.js
+++ sketch.js
@@ -1,36 +1,29 @@
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  earth = new Planet(100, height, 10, 0.005, color(47, 82, 229));
+  moon = new Planet(50, height, 3, 0.005, color(200, 200, 200));
 }
 
 function draw() {
   background(50);
-  noFill();
-  stroke(255);
-  translate(width / 2, height)
+  translate(width / 2, height * 1.25);
 
-  rectMode("center")
+  // rectMode("center");
 
   // Sun
-  // ellipse(0, 0, width / 2);
-  rect(0, 0, width / 2);
+  fill(255, 204, 0);
+  noStroke();
+  ellipse(0, 0, width / 1.5);
 
-  rotate(frameCount / -100.0);
+  earth.show();
+  earth.orbit();
+  moon.show();
+  moon.orbit();
+
+  // rotate(frameCount / -100.0);
   // polygon(0,-100, 50, 4);
-  rect(0, -200, 100, 100)
+  // rect(0, -200, 100, 100);
 
   // rotate(i * 1.5);
   // rect(250, 250, 50, 50);
 }
-
-function polygon(x, y, radius, npoints) {
-  let angle = TWO_PI / npoints;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius;
-    let sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
-
-const drawTriangle = () => {}
