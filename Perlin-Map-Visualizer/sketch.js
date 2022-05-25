26a951ea9f772143648e51a08c245e1cca6ca27f
Arthur P <44344292+arthur-pte@users.noreply.github.com>
2022-05-25 16:58:02 -0300

    Updates PMV

--- Perlin-Map-Visualizer/sketch.js
+++ Perlin-Map-Visualizer/sketch.js
@@ -1,96 +1,62 @@
+var pixelSizeSlider = document.getElementById("pixelSizeSlider");
+var pixelSizeLabel = document.getElementById("pixelSizeLabel");
 
-let noiseValue;
-const noiseScale = .015;
-const noiseOctaves = 4; // Number of octaves aka level of details
+pixelSizeLabel.innerHTML = 5;
+
+pixelSizeSlider.oninput = function () {
+    pixelSize = parseInt(this.value);
+    pixelSizeLabel.innerHTML = this.value;
+    redraw();
+}
+
+
+const seed = 5; // Seed noise
+const noiseZoom = .015;  // Scale of the noise
+const noiseOctaves = 4;  // Number of octaves aka level of details (LoD)
 const noiseFalloff = .5; // Falloff of the strengh of succeccives octaves (1 -> 0.5 -> 0.25 etc.)
-
-
-let min = 300;
-let max = 0;
-let moyenne = 0;
-let totalPixels = 0;
+let pixelSize = 5; // Size of a pixel
 
 
 function setup() {
-    createCanvas(window.innerWidth / 4, window.innerWidth / 4);
-
-    // noiseSeed(seed);
-
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            noiseDetail(noiseOctaves, noiseFalloff);
-            noiseValue = noise(x * noiseScale, y * noiseScale);
-            pixelHeight = noiseValue * 255;
-
-            if (noiseValue < min)
-                min = noiseValue;
-            if (noiseValue > max)
-                max = noiseValue;
-            moyenne += noiseValue;
-
-            totalPixels += 1;
-
-            if (pixelHeight < 50)       // Eau profonde
-                stroke(25, 25, 255);
-            else if (pixelHeight < 90)  // Eau peu profonde
-                stroke(75, 75, 255);
-            else if (pixelHeight < 100)  // Sable
-                stroke(255, 255, 0);
-            else if (pixelHeight < 150) // Herbe
-                stroke(0, 200, 0);
-            else if (pixelHeight < 180) // Montagnes
-                stroke(100, 100, 100);
-            else                        // Neige
-                stroke(255, 255, 255);
-
-            // stroke(pixelHeight);
-
-            point(x, y);
-        }
-    }
-    console.log("min : " + min * 255);
-    console.log("max : " + max * 255);
-    console.log("moyenne : " + (moyenne / totalPixels) * 255);
-    // moyenne = 0;
-    // totalPixels = 0;
+    createCanvas(window.innerWidth / 3, window.innerWidth / 3);
+    noLoop();
 }
 
+
 function draw() {
-    // background(0);
-    // for (let y = 0; y < height; y++) {
-    //     for (let x = 0; x < width; x++) {
-    //         noiseDetail(noiseOctaves, noiseFalloff);
-    //         noiseValue = noise(x * noiseScale, y * noiseScale);
-    //         pixelHeight = noiseValue * 255;
+    // ######################### DEBUG #########################
+    let drawTime = millis()                                // #
+    // #########################################################  
 
-    //         if (noiseValue < min)
-    //             min = noiseValue;
-    //         if (noiseValue > max)
-    //             max = noiseValue;
+    background(255);
 
-    //         moyenne += noiseValue;
-    //         totalPixels += 1;
+    noiseSeed(seed);
+    noiseDetail(noiseOctaves, noiseFalloff);
+    noStroke();
 
-    //         if (pixelHeight < 50)
-    //             stroke(0, 0, 255);
-    //         else if (pixelHeight < 100)
-    //             stroke(255, 255, 0);
-    //         else if (pixelHeight < 150)
-    //             stroke(0, 255, 0);
-    //         else if (noiseValue < 175)
-    //             stroke(100, 100, 100);
-    //         else
-    //             stroke(255, 255, 255);
+    for (let y = 0; y < height; y += pixelSize) {
+        for (let x = 0; x < width; x += pixelSize) {
+            pixelValue = 255 * noise(x * noiseZoom, y * noiseZoom);
 
-    //         // stroke(pixelHeight);
+            if (pixelValue < 50)       // Deeper water
+                fill(50, 50, 255);
+            else if (pixelValue < 90)  // Deep water
+                fill(100, 100, 255);
+            else if (pixelValue < 100) // Sand
+                fill(255, 255, 0);
+            else if (pixelValue < 150) // Grass
+                fill(0, 200, 0);
+            else if (pixelValue < 180) // Mountains
+                fill(100, 100, 100);
+            else                        // Snow
+                fill(255, 255, 255);
 
-    //         point(x, y);
-    //     }
-    // }
-    // console.log("min : " + min * 255);
-    // console.log("max : " + max * 255);
-    // console.log("moyenne : " + (moyenne / totalPixels) * 255);
-    // console.log("")
-    // moyenne = 0;
-    // totalPixels = 0;
+            rect(x, y, pixelSize);
+        }
+    }
+
+    // ######################################### DEBUG ##########################################
+    let endTime = millis();                                                                  // #
+    console.log("Duration since draw start : " + (endTime - drawTime).toFixed(2) + " ms\n"); // #
+    // ##########################################################################################
 }
