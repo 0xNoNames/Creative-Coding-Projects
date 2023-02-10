47af72204a99e02635d444d986a2d2b84a6d8a28
Arthur Pêtre <arthur30700@gmail.com>
2023-02-10 02:55:46 +0100

    Update sketch.js

--- Triangles-Doodle/sketch.js
+++ Triangles-Doodle/sketch.js
@@ -6,19 +6,52 @@ let offset = 2;
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(0);
-
+  translate(centerX, centerY);
   noFill();
   stroke(255);
 
+  noStroke();
+  fill("red");
+  x1 = 0, y1 = 0;
+  x2 = 0, y2 = size;
+  x3 = sin(PI / 3) * size, y3 = cos(PI / 3) * size;
 
-  triangle(centerX, centerY,
-    centerX + size, centerY,
-    centerX, centerY - size);
+  ellipse(x1, y1, 10);
+  ellipse(x2, y2, 10);
+  ellipse(x3, y3, 10);
 
-  stroke("red");
-  triangle(centerX, centerY,
-    centerX + size - (size / offset), centerY - (size / offset),
-    centerX, centerY - size + (size / offset));
+  triangle(x1, y1, x2, y2, x3, y3);
+
+  fill("green");
+  x1 = 0, y1 = size;
+  x2 = 0, y2 = size * 2;
+  x3 = sin(PI / 3) * size, y3 = cos(PI / 3) * size * 3;
+
+  ellipse(x1, y1, 10);
+  ellipse(x2, y2, 10);
+  ellipse(x3, y3, 10);
+
+  triangle(x1, y1, x2, y2, x3, y3);
+
+  fill("blue");
+  x1 = 0, y1 = size;
+  x2 = sin(PI / 3) * size, y2 = cos(PI / 3) * size;
+  x3 = sin(PI / 3) * size, y3 = cos(PI / 3) * size * 3;
+
+  ellipse(x1, y1, 10);
+  ellipse(x2, y2, 10);
+  ellipse(x3, y3, 10);
+
+  triangle(x1, y1, x2, y2, x3, y3);
+
+  // triangle(centerX, centerY,
+  //   centerX + size, centerY,
+  //   centerX, centerY - size);
+
+  // stroke("red");
+  // triangle(centerX, centerY,
+  //   centerX + size - (size / offset), centerY - (size / offset),
+  //   centerX, centerY - size + (size / offset));
 
 
   // stroke("blue");
@@ -27,15 +60,15 @@ function setup() {
   //   centerX, centerY - size + offset);
 
 
-  let AD = sqrt((size / offset) ** 2 + (2 * size / offset) ** 2);
-  let alpha = atan((size / offset) / (2 * size / offset));
-  let x = cos(alpha) * (AD / offset);
-  let y = sin(alpha) * (AD / offset);
+  // let AD = sqrt((size / offset) ** 2 + (2 * size / offset) ** 2);
+  // let alpha = atan((size / offset) / (2 * size / offset));
+  // let x = cos(alpha) * (AD / offset);
+  // let y = sin(alpha) * (AD / offset);
 
-  console.log(alpha)
-  noStroke();
-  fill("green");
-  ellipse(centerX + x, centerY - y, 10);
+  // console.log(alpha);
+  // noStroke();
+  // fill("green");
+  // ellipse(centerX + x, centerY - y, 10);
 }
 
 
