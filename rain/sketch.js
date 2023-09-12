70bc353f991dd1367fdfd38d4fbfed42cd680092
Arthur Pêtre <arthur30700@gmail.com>
2023-09-12 19:02:25 +0200

    Added dat gui cursors

--- rain/sketch.js
+++ rain/sketch.js
@@ -2,26 +2,44 @@ let gui = new dat.GUI();
 
 let variables = {
 	depth: 10,
-	distantOpacity: 100,
+	opacity: 100,
 	maxAngle: window.innerWidth > 2000 ? 40 : window.innerWidth * 0.02,
 	volume: 0.5,
-	numRaindrops: window.innerWidth,
-	speed: 1
+	numDrops: window.innerWidth,
+	slowMotion: 1,
+	minLenght: 3,
+	maxLenght: 100,
+	minWidth: 1,
+	maxWidth: 20,
+	minSpeed: 3,
+	maxSpeed: 60,
 };
-
-gui.add(variables, "depth", 0, 15);
-gui.add(variables, "distantOpacity", 0, 255);
-gui.add(variables, "maxAngle", 0, 90).listen();
-gui.add(variables, "volume", 0, 1);
-gui.add(variables, "numRaindrops", 0, 5000).listen();
-gui.add(variables, "speed", 0, 1);
 
 let palette = {
 	background: [13, 13, 20],
 	rain: [82, 104, 191],
 };
-gui.addColor(palette, "background");
-gui.addColor(palette, "rain");
+
+let lenghtFolder = gui.addFolder("Drop lenght");
+let widthFolder = gui.addFolder("Drop width");
+let speedFolder = gui.addFolder("Drop speed");
+let distantFolder = gui.addFolder("Distant drops");
+let colorsFolder = gui.addFolder("Colors");
+
+distantFolder.add(variables, "depth", 0, 15);
+distantFolder.add(variables, "opacity", 0, 255);
+lenghtFolder.add(variables, "minLenght", 0, 50);
+lenghtFolder.add(variables, "maxLenght", 0, 500);
+widthFolder.add(variables, "minWidth", 0, 10);
+widthFolder.add(variables, "maxWidth", 0, 60);
+speedFolder.add(variables, "minSpeed", 0, 20);
+speedFolder.add(variables, "maxSpeed", 0, 120);
+gui.add(variables, "maxAngle", 0, 90).listen();
+gui.add(variables, "numDrops", 0, 5000).listen();
+gui.add(variables, "volume", 0, 1);
+gui.add(variables, "slowMotion", 0, 1);
+colorsFolder.addColor(palette, "background");
+colorsFolder.addColor(palette, "rain");
 
 raindrops = [];
 
@@ -36,10 +54,10 @@ class Raindrop {
 		this.x = random(-width / 4, width * 1.25);
 		this.y = random(-height);
 		this.z = exp(random(0, variables.depth));
-		this.lenght = map(this.z, 1, maxDepth, 5, 100);
-		this.width = map(this.z, 1, maxDepth, 1, 20);
-		this.speed = map(this.z, 1, maxDepth, 3, 60);
-		this.opacity = map(this.z, 1, maxDepth, variables.distantOpacity, 255);
+		this.lenght = map(this.z, 1, maxDepth, variables.minLenght, variables.maxLenght);
+		this.width = map(this.z, 1, maxDepth, variables.minWidth, variables.maxWidth);
+		this.speed = map(this.z, 1, maxDepth, variables.minSpeed, variables.maxSpeed);
+		this.opacity = map(this.z, 1, maxDepth, variables.opacity, 255);
 	};
 
 	show = (angle) => {
@@ -49,8 +67,8 @@ class Raindrop {
 	};
 
 	update = (angle) => {
-		this.x += (this.speed * (1.5 * variables.speed)) * sin(angle);
-		this.y += (this.speed * (1.5 * variables.speed)) * cos(angle);
+		this.x += (this.speed * (1.5 * variables.slowMotion)) * sin(angle);
+		this.y += (this.speed * (1.5 * variables.slowMotion)) * cos(angle);
 		if (this.y > height)
 			this.initialize();
 	};
@@ -66,7 +84,7 @@ setup = () => {
 	strokeCap(SQUARE);
 	angleMode(DEGREES);
 	time = random(100);
-	for (let i = 0; i < variables.numRaindrops; i++)
+	for (let i = 0; i < variables.numDrops; i++)
 		raindrops.push(new Raindrop());
 };
 
@@ -81,15 +99,15 @@ draw = () => {
 		drop.show(angle);
 	});
 
-	time += 0.002 * (1.5 * variables.speed);
+	time += 0.002 * (1.5 * variables.slowMotion);
 
-	while (raindrops.length < variables.numRaindrops)
+	while (raindrops.length < variables.numDrops)
 		raindrops.push(new Raindrop());
 
-	while (raindrops.length > variables.numRaindrops)
+	while (raindrops.length > variables.numDrops)
 		raindrops.pop();
 
-	rainSound.rate(variables.speed);
+	rainSound.rate(variables.slowMotion);
 	rainSound.setVolume(variables.volume);
 };
 
@@ -97,7 +115,7 @@ draw = () => {
 windowResized = () => {
 	resizeCanvas(windowWidth, windowHeight);
 	variables.maxAngle = map(width, 0, 2000, 0, 40);
-	variables.numRaindrops = width;
+	variables.numDrops = width;
 };
 
 
